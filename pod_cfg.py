2d034e9f29237ad6781465893fc619fdb37027c8
lilinj2000 <lilinj2000@gmail.com>
2016-02-17 13:28:13 +0800

    s done.

--- pod_cfg.py
+++ pod_cfg.py
@@ -4,14 +4,14 @@ import json
 
 input_pack = { 
     "source": "172.31.0.4", 
-    "destination": "172.31.0.3",
-    "size": 266
+    "destination": "172.31.0.5",
+    "size": 171
     }
 
 output_pack = {
-    "source": "10.47.109.57", 
+    "source": "10.47.109.59", 
     "destination": "192.168.11.131",
-    "size": 269
+    "size": 278
     }
 
 timestamp = {
@@ -19,11 +19,11 @@ timestamp = {
     "checksum": "0xc3"
     }
 
-config = {"data_file": "data/case_0126_1430_x.pcap", 
+config = {"data_file": "data/case_0217_1310_s.pcap", 
           "input": input_pack, 
           "output":output_pack, 
           "timestamp": timestamp, 
-          "output_file": "data/up_delay_x_0126_1430.data"}
+          "output_file": "data/up_delay_s_0217_1310.data"}
 
 with open('pod.cfg', 'w') as f:
     json.dump(config, f, sort_keys=True, indent=4)
