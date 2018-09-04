02559b982568feef1e5036fe28eaad4f199d346e
lilinj2000 <lilinj2000@gmail.com>
2018-09-04 17:25:00 +0800

    done

--- pod_metawatch/metawatch_cfg.py
+++ pod_metawatch/metawatch_cfg.py
@@ -23,7 +23,7 @@ t2_pack = {
     "src_ip": "220.248.39.174",
     "dst_ip": "192.168.230.243",
     "src_port": 33005,
-    "size": [192, 203]
+    "size": [192, 194, 203, 205, 320, 324, 328, 330, 451, 574]
     }
 
 t3_pack = {
@@ -31,15 +31,15 @@ t3_pack = {
     "src_ip": "192.168.230.139",
     "dst_ip": "192.168.230.129",
     "src_port": 8002,
-    "size": [242, 253]
+    "size": [239, 242, 249, 250, 251, 252, 253, 254, 418, 420, 426]
     }
 
-config = {"pcap_file": "./metawatch_ciara_20180903_002.pcap", 
+config = {"pcap_file": "./metawatch_ciara_20180903_004.pcap", 
           "t0": t0_pack,
           "t1": t1_pack, 
           "t2": t2_pack, 
           "t3": t3_pack,
-          "output_file": "./metawatch_ciara_20180903_002.data"}
+          "output_file": "./metawatch_ciara_20180903_004.data"}
 
 with open('ciara_metawatch.cfg', 'w') as f:
     json.dump(config, f, sort_keys=True, indent=4)
--- pod_metawatch/pod_metawatch.py
+++ pod_metawatch/pod_metawatch.py
@@ -7,7 +7,48 @@ import datetime
 import socket
 import json
 import argparse
+import os
 
+def sort(data_file):
+    cmd_sort = 'sort -k 2n,2n -k 3,3 %s > %s.sort' % (data_file, data_file)
+    os.system(cmd_sort)
+
+def merge(data_file):
+    f_output = open('%s.merge' % data_file, "w")
+
+    pre_order = -1
+    cur_order = -1
+    exp_ts_flag = 0
+    ts_list = []
+    with open('%s.sort' % data_file) as f:
+        for line in f:
+            fields = line.split()
+            # no = int(fields[0])
+            order = int(fields[1])
+            ts_flag = int(fields[2][1])
+            ts = fields[3]
+            
+            if cur_order != order:
+                pre_order = cur_order
+                cur_order = order
+            
+                if len(ts_list) ==0 or len(ts_list) == 4:
+                    pass
+                else:
+                    print("Error: order %d" % pre_order)
+                    exit()
+
+            if exp_ts_flag % 4 == ts_flag:
+                ts_list.append(ts)
+                exp_ts_flag = exp_ts_flag + 1
+
+                if ts_flag == 3:
+                    f_output.write('\t'.join(ts_list))
+                    f_output.write('\n')
+                    ts_list = []
+            # if exp_ts_flag == ts_flag:
+            #     f_output.write('%s' % ts)
+        f.close
 
 def read_config(cfg_file):
     with open(cfg_file) as f:
@@ -28,7 +69,9 @@ def filter_packets(pcap, config):
     f_output = open(config["output_file"], "w") 
 
     # For each packet in the pcap process the contents
+    no = 0
     for _, buf in pcap:
+        no = no +1
 
         # Unpack the Ethernet frame (mac src/dst, ethertype)
         eth = dpkt.ethernet.Ethernet(buf)
@@ -67,43 +110,96 @@ def filter_packets(pcap, config):
             and ip_dst == config["t0"]["dst_ip"] \
             and port_dst == config["t0"]["dst_port"] \
             and size in config["t0"]["size"]:
-            f_output.write('%d.%09d' % (hw_second, hw_ns))
-
+            
+            ts = 't0'
             order_local_id = int(''.join('%s' % chr(ord(x)) for x in buf[size-181:size-161]), 10)
-            print("t0 order_local_id = %d" % order_local_id)
+            # print("No %d - t0 order_local_id = %d" % (no,order_local_id))
             # print("%d => %d" % (port_src, port_dst))
         elif ord(source) == config["t1"]["source"] \
             and ip_src == config["t1"]["src_ip"] \
             and ip_dst == config["t1"]["dst_ip"] \
             and port_dst == config["t1"]["dst_port"] \
             and size in config["t1"]["size"]:
-            f_output.write('\t%d.%09d' % (hw_second, hw_ns))
+            # f_output.write('\t%d.%09d\n' % (hw_second, hw_ns))
             
+            ts = 't1'
             order_local_id = int(''.join('%s' % chr(ord(x)) for x in buf[size-59:size-46]), 10)
-            print("t1 order_local_id = %d" % order_local_id)
+            # print("No %d - t1 order_local_id = %d" % (no, order_local_id))
         elif ord(source) == config["t2"]["source"] \
             and ip_src == config["t2"]["src_ip"] \
             and ip_dst == config["t2"]["dst_ip"] \
             and port_src == config["t2"]["src_port"] \
             and size in config["t2"]["size"]:
-            f_output.write('\t%d.%09d' % (hw_second, hw_ns))
     
+            ts = 't2'
+            if size == 320:
+                pre_local_id = int(''.join('%s' % chr(ord(x)) for x in buf[size-153:size-140]), 10)
+                f_output.write('%d\t%d\t%s\t%d.%09d\n' % (no, pre_local_id, ts, hw_second, hw_ns))
+
+            if size == 324:
+                pre_local_id = int(''.join('%s' % chr(ord(x)) for x in buf[size-155:size-142]), 10)
+                f_output.write('%d\t%d\t%s\t%d.%09d\n' % (no, pre_local_id, ts, hw_second, hw_ns))
+            
+            if size == 328 or size == 451 \
+                or size == 574:
+                pre_local_id = int(''.join('%s' % chr(ord(x)) for x in buf[size-157:size-144]), 10)
+                f_output.write('%d\t%d\t%s\t%d.%09d\n' % (no, pre_local_id, ts, hw_second, hw_ns))
+            
+            if size == 330:
+                pre_local_id = int(''.join('%s' % chr(ord(x)) for x in buf[size-159:size-146]), 10)
+                f_output.write('%d\t%d\t%s\t%d.%09d\n' % (no, pre_local_id, ts, hw_second, hw_ns))
+            
+            if size == 451 or size == 574:
+                pre_local_id = int(''.join('%s' % chr(ord(x)) for x in buf[size-280:size-267]), 10)
+                f_output.write('%d\t%d\t%s\t%d.%09d\n' % (no, pre_local_id, ts, hw_second, hw_ns))
+            
+            if size == 574:
+                pre_local_id = int(''.join('%s' % chr(ord(x)) for x in buf[size-403:size-390]), 10)
+                f_output.write('%d\t%d\t%s\t%d.%09d\n' % (no, pre_local_id, ts, hw_second, hw_ns))
+            
             order_local_id = int(''.join('%s' % chr(ord(x)) for x in buf[size-34:size-21]), 10)
-            print("t2 order_local_id = %d" % order_local_id)
+            
+            # if size == 328:
+            #     print('size is 328, no is %d' % no)
+            # elif size == 194:
+            #     order_local_id = int(''.join('%s' % chr(ord(x)) for x in buf[size-32:size-19]), 10)
+            
+            # print("No %d - t2 order_local_id = %d" % (no, order_local_id))
         elif ord(source) == config["t3"]["source"] \
             and ip_src == config["t3"]["src_ip"] \
             and ip_dst == config["t3"]["dst_ip"] \
             and port_src == config["t3"]["src_port"] \
             and size in config["t3"]["size"]:
-            f_output.write('\t%d.%09d\n' % (hw_second, hw_ns))
-            if size == 242:
+            # f_output.write('\t%d.%09d\n' % (hw_second, hw_ns))
+            ts = 't3'
+
+            if size == 418:
+                pre_local_id = int(''.join('%s' % chr(ord(x)) for x in buf[size-295:size-275]), 10)
+                f_output.write('%d\t%d\t%s\t%d.%09d\n' % (no, pre_local_id, ts, hw_second, hw_ns))
+
+            if size == 420:
+                pre_local_id = int(''.join('%s' % chr(ord(x)) for x in buf[size-296:size-276]), 10)
+                f_output.write('%d\t%d\t%s\t%d.%09d\n' % (no, pre_local_id, ts, hw_second, hw_ns))
+            
+            if size == 426:
+                pre_local_id = int(''.join('%s' % chr(ord(x)) for x in buf[size-299:size-279]), 10)
+                f_output.write('%d\t%d\t%s\t%d.%09d\n' % (no, pre_local_id, ts, hw_second, hw_ns))
+
+            if size == 139 or size == 242:
                 order_local_id = int(''.join('%s' % chr(ord(x)) for x in buf[size-116:size-96]), 10)
-            elif size == 253:
+            elif size == 249 or size == 250 \
+                or size == 251 \
+                or size == 252 or size == 253 \
+                or size == 254 \
+                or size == 418 or size == 420 \
+                or size == 426:
                 order_local_id = int(''.join('%s' % chr(ord(x)) for x in buf[size-127:size-107]), 10)
             
-            print("t3 order_local_id = %d" % order_local_id)
+            # print("No %d - t3 order_local_id = %d" % (no, order_local_id))
         else:
             continue
+        
+        f_output.write('%d\t%d\t%s\t%d.%09d\n' % (no, order_local_id, ts, hw_second, hw_ns))
         
 
         # Print out the ts in UTC
@@ -127,6 +223,9 @@ def main():
         pcap = dpkt.pcap.Reader(f)
         filter_packets(pcap, config)
 
+    sort(config['output_file'])
+
+    merge(config['output_file'])
 
 if __name__ == '__main__':
     main()
