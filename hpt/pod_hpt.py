19b8696405bc3ed881bc0d8c8d3bc54e58e96461
lilinj2000 <lilinj2000@gmail.com>
2018-10-31 11:07:34 +0800

    the output format to json

--- hpt/pod_hpt.py
+++ hpt/pod_hpt.py
@@ -4,24 +4,31 @@
 """
 import dpkt
 import argparse
+import json
+import sys
 
 def filter_packets(pcap):
 
     # For each packet in the pcap process the contents
     no = 0
+    
     for _, buf in pcap:
+        dict_ts_trailer = {}
+        
         no = no +1
+        dict_ts_trailer["no"] = no
+        
 
         size = len(buf)
 
         # Unpack the Ethernet frame (mac src/dst, ethertype)
-        eth = dpkt.ethernet.Ethernet(buf)
+        # eth = dpkt.ethernet.Ethernet(buf)
         # print 'Ethernet Frame: ', mac_addr(eth.src), mac_addr(eth.dst), eth.type
 
         # Make sure the Ethernet frame contains an IP packet
         # EtherType (IP, ARP, PPPoE, IP6... see http://en.wikipedia.org/wiki/EtherType)
-        if eth.type != dpkt.ethernet.ETH_TYPE_IP:
-            continue
+        # if eth.type != dpkt.ethernet.ETH_TYPE_IP:
+        #     continue
         
         # ip = eth.data
         # if ip.p != dpkt.ip.IP_PROTO_TCP:
@@ -31,30 +38,52 @@ def filter_packets(pcap):
         # port_src = tcp.sport
         # port_dst = tcp.dport
 
+        dict_exablaze_ts_trailer = {}
         exablaze_ts_trailer = buf[size-16:]
         ts_hex_dump = ' '.join('%02x' % ord(x) for x in exablaze_ts_trailer)
-        print('no %d, exablaze_ts_trailer: \n %s' % (no, ts_hex_dump))
+        dict_exablaze_ts_trailer["hex"] = ts_hex_dump
+        # print('no %d, exablaze_ts_trailer: \n %s' % (no, ts_hex_dump))
 
         original_fcs = ' '.join('%02x' % ord(x) for x in exablaze_ts_trailer[0:4])
-        print('original_fcs: %s' % original_fcs)
+        # print('original_fcs: %s' % original_fcs)
+        dict_exablaze_ts_trailer["original_fcs"] = original_fcs
 
         device_id = exablaze_ts_trailer[4]
-        print('device_id: %d <==> %02x' % (ord(device_id), ord(device_id)))
+        # print('device_id: %d <==> %02x' % (ord(device_id), ord(device_id)))
+        dict_exablaze_ts_trailer["device_id"] = ord(device_id)
 
         port = exablaze_ts_trailer[5]
-        print('port: %d <==> %02x' % (ord(port), ord(port)))
+        # print('port: %d <==> %02x' % (ord(port), ord(port)))
+        dict_exablaze_ts_trailer["port"] = ord(port)
 
         seconds_since_epoch_hex_dump = ' '.join('%02x' % ord(x) for x in exablaze_ts_trailer[6:10])
         seconds_since_epoch = int(''.join('%02x' % ord(x) for x in exablaze_ts_trailer[6:10]), 16)
-        print('seconds_since_epoch  %d <==> %s' % (seconds_since_epoch, seconds_since_epoch_hex_dump))
+        # print('seconds_since_epoch  %d <==> %s' % (seconds_since_epoch, seconds_since_epoch_hex_dump))
+        dict_seconds_since_epoch = {}
+        dict_seconds_since_epoch["hex"] = seconds_since_epoch_hex_dump
+        dict_seconds_since_epoch["dec"] = seconds_since_epoch
+        dict_exablaze_ts_trailer["seconds_since_epoch"] = dict_seconds_since_epoch
+    
 
         frac_seconds_hex_dump = ' '.join('%02x' % ord(x) for x in exablaze_ts_trailer[10:15])
         frac_seconds = int(''.join('%02x' % ord(x) for x in exablaze_ts_trailer[10:15]), 16)
         frac_nano_seconds = frac_seconds * 2**-40 * 10**9
-        print('frac_seconds %d <==> %s ==> %09d(ns)' % (frac_seconds, frac_seconds_hex_dump, frac_nano_seconds))
+        dict_frac_seconds = {}
+        dict_frac_seconds["dec"] = frac_seconds
+        dict_frac_seconds["hex"] = frac_seconds_hex_dump
+        dict_frac_seconds["nao_second"] = frac_nano_seconds
+        dict_exablaze_ts_trailer["frac_seconds"] = dict_frac_seconds
 
         _reserved = exablaze_ts_trailer[15]
-        print('_reserved: %02x' % ord(_reserved))
+        # print('_reserved: %02x' % ord(_reserved))
+        dict_exablaze_ts_trailer["reserved"] = ord(_reserved)
+
+        dict_ts_trailer["exablaze_ts_trailer"] = dict_exablaze_ts_trailer
+
+        json.dump(dict_ts_trailer, sys.stdout, sort_keys=True, indent=4)
+        print('')
+
+        # break
         
 def main():
     
--- metawatch/pod_metawatch.py
+++ metawatch/pod_metawatch.py
@@ -4,24 +4,29 @@
 """
 import dpkt
 import argparse
+import json
+import sys
 
 def filter_packets(pcap):
 
     # For each packet in the pcap process the contents
     no = 0
     for _, buf in pcap:
+        dict_ts_trailer = {}
+        
         no = no +1
-
+        dict_ts_trailer["no"] = no
+        
         size = len(buf)
 
         # Unpack the Ethernet frame (mac src/dst, ethertype)
-        eth = dpkt.ethernet.Ethernet(buf)
+        # eth = dpkt.ethernet.Ethernet(buf)
         # print 'Ethernet Frame: ', mac_addr(eth.src), mac_addr(eth.dst), eth.type
 
         # Make sure the Ethernet frame contains an IP packet
         # EtherType (IP, ARP, PPPoE, IP6... see http://en.wikipedia.org/wiki/EtherType)
-        if eth.type != dpkt.ethernet.ETH_TYPE_IP:
-            continue
+        # if eth.type != dpkt.ethernet.ETH_TYPE_IP:
+        #     continue
         
         # ip = eth.data
         # if ip.p != dpkt.ip.IP_PROTO_TCP:
@@ -31,31 +36,53 @@ def filter_packets(pcap):
         # port_src = tcp.sport
         # port_dst = tcp.dport
 
+        dict_metamako_ts_trailer = {}
         metawatch_ts_trailer = buf[size-16:]
         ts_hex_dump = ' '.join('%02x' % ord(x) for x in metawatch_ts_trailer)
-        print('no %d, metawatch_ts_trailer: \n %s' % (no, ts_hex_dump))
+        dict_metamako_ts_trailer["hex"] = ts_hex_dump
+        # print('no %d, metawatch_ts_trailer: \n %s' % (no, ts_hex_dump))
 
         original_fcs = ' '.join('%02x' % ord(x) for x in metawatch_ts_trailer[0:4])
-        print('original_fcs: %s' % original_fcs)
+        # print('original_fcs: %s' % original_fcs)
+        dict_metamako_ts_trailer["original_fcs"] = original_fcs
 
         seconds_since_epoch_hex_dump = ' '.join('%02x' % ord(x) for x in metawatch_ts_trailer[4:8])
         seconds_since_epoch = int(''.join('%02x' % ord(x) for x in metawatch_ts_trailer[4:8]), 16)
-        print('seconds_since_epoch  %d <==> %s' % (seconds_since_epoch, seconds_since_epoch_hex_dump))
+        # print('seconds_since_epoch  %d <==> %s' % (seconds_since_epoch, seconds_since_epoch_hex_dump))
+        dict_seconds_since_epoch = {}
+        dict_seconds_since_epoch["hex"] = seconds_since_epoch_hex_dump
+        dict_seconds_since_epoch["dec"] = seconds_since_epoch
+        dict_metamako_ts_trailer["seconds_since_epoch"] = dict_seconds_since_epoch
 
-        frac_seconds_hex_dump = ' '.join('%02x' % ord(x) for x in metawatch_ts_trailer[8:12])
-        frac_seconds = int(''.join('%02x' % ord(x) for x in metawatch_ts_trailer[8:12]), 16)
-        print('frac_seconds %d <==> %s' % (frac_seconds, frac_seconds_hex_dump))
+        nano_seconds_hex_dump = ' '.join('%02x' % ord(x) for x in metawatch_ts_trailer[8:12])
+        nano_seconds = int(''.join('%02x' % ord(x) for x in metawatch_ts_trailer[8:12]), 16)
+        # print('nano_seconds %d <==> %s' % (nano_seconds, nano_seconds_hex_dump))
+        dict_nano_seconds = {}
+        dict_nano_seconds["dec"] = nano_seconds
+        dict_nano_seconds["hex"] = nano_seconds_hex_dump
+        dict_metamako_ts_trailer["nano_seconds"] = dict_nano_seconds
 
         flags = metawatch_ts_trailer[12]
-        print('flags: %d <==> %02x' % (ord(flags), ord(flags)))
+        # print('flags: %d <==> %02x' % (ord(flags), ord(flags)))
+        dict_metamako_ts_trailer["flags"] = ord(flags)
 
         device_id_hex_dump = ' '.join('%02x' % ord(x) for x in metawatch_ts_trailer[13:15])
         device_id = int(''.join('%02x' % ord(x) for x in metawatch_ts_trailer[13:15]), 16)
-        print('device_id: %d <==> %s' % (device_id, device_id_hex_dump))
+        # print('device_id: %d <==> %s' % (device_id, device_id_hex_dump))
+        dict_device_id = {}
+        dict_device_id["dec"] = device_id
+        dict_device_id["hex"] = device_id_hex_dump
+        dict_metamako_ts_trailer["device_id"] = dict_device_id
 
         port = metawatch_ts_trailer[15]
-        print('port: %d <==> %02x' % (ord(port), ord(port)))
+        # print('port: %d <==> %02x' % (ord(port), ord(port)))
+        dict_metamako_ts_trailer["port"] = ord(port)
 
+        dict_ts_trailer["metamako_ts_trailer"] = dict_metamako_ts_trailer
+
+        json.dump(dict_ts_trailer, sys.stdout, sort_keys=True, indent=4)
+        print('')
+        
         # break
 
         
